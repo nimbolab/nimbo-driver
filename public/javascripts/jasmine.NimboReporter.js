1f53c04cf4160d0a668d406721d41c123a5ffcf2
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-05-26 22:44:22 +0200

    Jasmine reporter returns passed/failed counts

--- public/javascripts/jasmine.NimboReporter.js
+++ public/javascripts/jasmine.NimboReporter.js
@@ -1,29 +1,44 @@
-jasmine.NimboReporter = function() {
+jasmine.NimboReporter = function () {
 	// console.log('Reporter new NimboReporter');
 };
 
-jasmine.NimboReporter.prototype.reportRunnerStarting = function(runner) {
+jasmine.NimboReporter.prototype.reportRunnerStarting = function (runner) {
 	// console.log('Reporter reporterRunnerStarting', runner);
 };
 
-jasmine.NimboReporter.prototype.reportRunnerResults = function(runner) {
-	top.nimbo.spinner.postMessage('finished');	
-	// console.log('Reporter reportRunnerResults', runner);
+jasmine.NimboReporter.prototype.reportRunnerResults = function (runner) {
+	var suites, specs;
+	var failedCount = 0;
+	var passedCount = 0;
+
+	suites = runner.suites();
+	for (var suiteIndex = 0; suiteIndex < suites.length; suiteIndex++) {
+		specs = suites[suiteIndex].specs();
+		for (var specIndex = 0; specIndex < specs.length; specIndex++) {
+			failedCount += specs[specIndex].results().failedCount;
+			passedCount += specs[specIndex].results().passedCount;
+		}
+	}
+
+	top.nimbo.spinner.postMessage({
+		failed: failedCount,
+		passed: passedCount
+	});	
 };
 
-jasmine.NimboReporter.prototype.reportSuiteResults = function(suite) {
+jasmine.NimboReporter.prototype.reportSuiteResults = function (suite) {
 	// console.log('Reporter reportSuiteResults', suite);
 };
 
-jasmine.NimboReporter.prototype.reportSpecStarting = function(spec) {
+jasmine.NimboReporter.prototype.reportSpecStarting = function (spec) {
 	// console.log('Reporter reportSpecStarting', spec);
 };
 
-jasmine.NimboReporter.prototype.reportSpecResults = function(spec) {
+jasmine.NimboReporter.prototype.reportSpecResults = function (spec) {
 	// console.log('Reporter reportSpecResults', spec);
 };
 
-jasmine.NimboReporter.prototype.log = function(str) {
+jasmine.NimboReporter.prototype.log = function (str) {
 	// console.log('Reporter log', str);
 };
 
