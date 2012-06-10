1875be01d25ec952c46b0847018c7c3b0e551b27
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-10 16:50:24 +0200

    Cleaned driver code

--- nimbo_driver.rb
+++ nimbo_driver.rb
@@ -4,34 +4,17 @@ require 'json'
 
 class NimboDriver < Sinatra::Base
 
-  framework_files = {}
-  framework_files["jasmine"] = []
-  framework_files["jasmine"] << "jasmine.NimboReporter.js"
-
   suite_id = nil
   result = nil
- 
-  get '/' do
-    'home'
-  end
- 
-  get '/spinner' do
-    erb :spinner
-  end
- 
-  get '/suite_run/:id' do
-    result = nil
-    suite_id = params[:id]
-    config = YAML.load_file("public/suites/#{suite_id}/.nimbo.yml")
 
-    until result do end
-    { result: result }.to_json
+  get '/capture' do
+    erb :capture
   end
- 
+
   get '/suite_get' do
     suite_id.to_json
   end
- 
+
   get '/suite_runner' do
     config = YAML.load_file("public/suites/#{suite_id}/.nimbo.yml")
     @suite_id = suite_id
@@ -39,16 +22,16 @@ class NimboDriver < Sinatra::Base
     @framework_scripts = framework_files[config["framework"]]
     erb :runner
   end
- 
-  post '/suite_result' do
+
+  get '/suite_start/:id' do
+    result = nil
+    suite_id = params[:id]
+    until result do end
+    { result: result }.to_json
+  end
+
+  get '/suite_end' do
     result = params
     suite_id = nil
   end
-
-  get '/stop' do
-    suite_id = nil
-    result = {}
-	end
-
-  run! if app_file == $0	
 end
