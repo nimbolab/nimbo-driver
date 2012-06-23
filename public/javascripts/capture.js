1a5f1a381710610402a318bfe9afd2fbd1557719
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-23 20:36:07 +0200

    fixed indentation

--- public/javascripts/capture.js
+++ public/javascripts/capture.js
@@ -1,57 +1,57 @@
 var nimbo = nimbo || {};
 
 nimbo.capture = {
-  init: function () {
-    var that = this;
+    init: function () {
+        var that = this;
 
-    that.statusContainer = document.getElementById('status');
-    that.runnerContainer = document.getElementById('runner');
+        that.statusContainer = document.getElementById('status');
+        that.runnerContainer = document.getElementById('runner');
 
-    that._status = 'idle';
-    that._setStatus(that._status);
+        that._status = 'idle';
+        that._setStatus(that._status);
 
-    that._pollId = setInterval(function () {
-      that._getSuite();
-    }, 1000);
-  },
-  postMessage: function (data) {
-    this._onSuiteMessage(data);
-  },	
-  _getSuite: function () {
-    var that = this;
-    if (that._status === 'idle') {
-      that._setStatus('polling');
-      $.get('/suite_get', function (data) {
-        that._onSuite(data);
-      });
-    };
-  },
-  _onSuite: function (data) {
-    var suite_id = JSON.parse(data);
-    if (suite_id) {
-      this._setStatus('running');
-      this._runSuite(suite_id);
-    } else {
-      this._setStatus('idle');
+        that._pollId = setInterval(function () {
+            try {
+                if (that._status === 'idle') { that._getSuite(); }
+            } catch (e) {}
+        }, 1000);
+    },
+    postMessage: function (data) {
+        this._onSuiteMessage(data);
+    },
+    _getSuite: function () {
+        var that = this;
+        that._setStatus('polling');
+        $.get('/suite_get', function (data) {
+            that.suite_id = JSON.parse(data);
+            if (that.suite_id) {
+                that._onSuite(that.suite_id);
+            } else {
+                that._setStatus('idle');
+            }
+        });
+    },
+    _onSuite: function () {
+        this._setStatus('running');
+        this._runSuite();
+    },
+    _runSuite: function () {
+        var runner = document.createElement('iframe');
+        runner.src = "/suite_runner/" + this.suite_id;
+        this.runnerContainer.appendChild(runner);
+    },
+    _removeSuite: function () {
+        this.runnerContainer.innerHTML = "";
+    },
+    _onSuiteMessage: function (data) {
+        $.post('/suite_result/' + this.suite_id, { result: data });
+        this._removeSuite();
+        this._setStatus('idle');
+    },
+    _setStatus: function (status) {
+        this._status = status;
+        this.statusContainer.innerHTML = status;
     }
-  },
-  _runSuite: function (suite_id) {
-    var runner = document.createElement('iframe');
-    runner.src = "/suite_runner";
-    this.runnerContainer.appendChild(runner);
-  },
-  _removeSuite: function () {
-    this.runnerContainer.innerHTML = "";
-  },
-  _onSuiteMessage: function (data) {
-    this._setStatus('idle');
-    this._removeSuite();
-    $.post('/suite_result', data);
-  },
-  _setStatus: function (status) {
-    this._status = status;
-    this.statusContainer.innerHTML = status;
-  }
 };
 
 $(function () { nimbo.capture.init(); });
--- public/javascripts/jasmine.NimboReporter.js
+++ public/javascripts/jasmine.NimboReporter.js
@@ -1,45 +1,45 @@
 jasmine.NimboReporter = function () {
-	// console.log('Reporter new NimboReporter');
+    // console.log('Reporter new NimboReporter');
 };
 
 jasmine.NimboReporter.prototype.reportRunnerStarting = function (runner) {
-	// console.log('Reporter reporterRunnerStarting', runner);
+    // console.log('Reporter reporterRunnerStarting', runner);
 };
 
 jasmine.NimboReporter.prototype.reportRunnerResults = function (runner) {
-	var suites, specs;
-	var failedCount = 0;
-	var passedCount = 0;
+    var suites, specs;
+    var failedCount = 0;
+    var passedCount = 0;
 
-	suites = runner.suites();
-	for (var suiteIndex = 0; suiteIndex < suites.length; suiteIndex++) {
-		specs = suites[suiteIndex].specs();
-		for (var specIndex = 0; specIndex < specs.length; specIndex++) {
-			failedCount += specs[specIndex].results().failedCount;
-			passedCount += specs[specIndex].results().passedCount;
-		}
-	}
+    suites = runner.suites();
+    for (var suiteIndex = 0; suiteIndex < suites.length; suiteIndex++) {
+        specs = suites[suiteIndex].specs();
+        for (var specIndex = 0; specIndex < specs.length; specIndex++) {
+            failedCount += specs[specIndex].results().failedCount;
+            passedCount += specs[specIndex].results().passedCount;
+        }
+    }
 
-	top.nimbo.capture.postMessage({
-		failed: failedCount,
-		passed: passedCount
-	});	
+    top.nimbo.capture.postMessage({
+        failed: failedCount,
+        passed: passedCount
+    });
 };
 
 jasmine.NimboReporter.prototype.reportSuiteResults = function (suite) {
-	// console.log('Reporter reportSuiteResults', suite);
+    // console.log('Reporter reportSuiteResults', suite);
 };
 
 jasmine.NimboReporter.prototype.reportSpecStarting = function (spec) {
-	// console.log('Reporter reportSpecStarting', spec);
+    // console.log('Reporter reportSpecStarting', spec);
 };
 
 jasmine.NimboReporter.prototype.reportSpecResults = function (spec) {
-	// console.log('Reporter reportSpecResults', spec);
+    // console.log('Reporter reportSpecResults', spec);
 };
 
 jasmine.NimboReporter.prototype.log = function (str) {
-	// console.log('Reporter log', str);
+    // console.log('Reporter log', str);
 };
 
 jasmine.getEnv().addReporter(new jasmine.NimboReporter());
